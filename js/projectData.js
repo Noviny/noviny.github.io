ae5d5612d4e1829668296e940c3c0571778f65ca
Noviny BC <benjaminconolly@gmail.com>
2016-02-15 12:02:02 +1100

    images changed

--- js/projectData.js
+++ js/projectData.js
@@ -1,67 +1,52 @@
 var app = app || {}
 
 app.projectData = {
-  "winlanewingame" :
-    {
-      "name": "Win Lane, Win Game",
-      "url": "https://github.com/Noviny/WinLane-WinGame",
-      "screenshot": "A screenshot url in an image tag",
-      "what": "Dubbed 'Project 0', this was the first project I made at General Assembly. We were tasked with building a game. After building tic-tac-toe, I decided to design my own game to digitise.",
-      "why": "I have designed a number of card games in the past, and the chance to play with the components of a computer was incredibly exciting. It forced me to invent all the javascript logic myself, which made me intimately familiar with lots of data-manipulation.",
-      "lesson": "This project taught me a lot on how to structure files within a project to separate concerns, and allow functions to be developed in parallel between the display and the game logic.",
-      "forLater": "When I go back to this project, I want to add in a multiplayer option, which was beyond the scope of this project. With the tools I have learned since, adding a server would be relatively easy."
-    },
+  "winlanewingame": {
+    "name": "Win Lane, Win Game",
+    "url": "https://github.com/Noviny/WinLane-WinGame",
+    "screenshot": "A screenshot url in an image tag",
+    "what": "Dubbed 'Project 0', this was the first project I made at General Assembly. We were tasked with building a game. After building tic-tac-toe, I decided to design my own game to digitise.",
+    "why": "I have designed a number of card games in the past, and the chance to play with the components of a computer was incredibly exciting. It forced me to invent all the javascript logic myself, which made me intimately familiar with lots of data-manipulation. Making a game that had asynchronous information share was a fun challenge.",
+    "lesson": "This project taught me a lot on how to structure files within a project to separate concerns, and allow functions to be developed in parallel between the display and the game logic.",
+    "forLater": "When I go back to this project, I want to add in a multiplayer mode, which was beyond the scope of this project. With the tools I have learned since, adding a server would be relatively easy."
+  },
 
-  "bookmarksplus" :
-    {
-      "name": "Bookmarks+",
-      "url": "https://github.com/Noviny/journal-maker",
-      "screenshot": "A screenshot url in an image tag",
-      "what": "This app was built for 'project 1' at GA, and allows urls to be saved by a user in to 'books', which can then be sorted by 'chapter' and allow additional details to be added.",
-      "why": "I have been frustrated several times by knowing that there is great content on the internet, but I cannot share it because the archives are too dense to send someone to. This project evolved into a general-purpose way to collect and share bookmarks, to try and distill the massive internet archives.",
-      "lesson": "Properly structruring your Models Viewers and Controllers can make expressing even complex relationships much easier.",
-      "forLater": "I am currently working on a chrome extension for this project, to allow people to add exisitng folders swiftly from links a person has already collected and sorted."
-    },
+  "bookmarksplus": {
+    "name": "Bookmarks+",
+    "url": "https://github.com/Noviny/journal-maker",
+    "screenshot": "A screenshot url in an image tag",
+    "what": "This app was built for 'project 1' at GA, and allows urls to be saved by a user in to 'books', which can then be sorted by 'chapter' and allow additional details to be added.",
+    "why": "I have been frustrated several times by knowing that there is great content on the internet, but I cannot share it because the archives are too dense to send someone to. This project evolved into a general-purpose way to collect and share bookmarks, to try and distill the massive internet archives. Since the only information taken from a website are a title and a link, it can swiftly and legally allow someone to bind the equivalent of an academic journal for distribution.",
+    "lesson": "Properly structruring your Models Viewers and Controllers can make expressing even complex relationships much easier.",
+    "forLater": "I am currently working on a chrome extension for this project, to allow people to add exisitng folders swiftly from links a person has already collected and sorted."
+  },
 
-  "moneyistime" :
-    {
-      "name": "Money Is Time",
-      "url": "https://github.com/Noviny/money-is-time",
-      "screenshot": "A screenshot url in an image tag",
-      "what": "This small app allows a person to find out their hourly rate from any kind of salary, and from there discover how many minutes or hours particular purchases take to earn.",
-      "why": "I made this tiny tool over the Christmas break both to try and help people think about 'value' differently and to practice javascript after a large focus on ruby.",
-      "lesson": "Having a user to test your design, and give you feedback is incredibly important. Thanks to my mum for taking an interest.",
-      "forLater": "This app was designed to be a very small tool and does not need its scope extended. If I wanted to practice my styling, this might be a site worth working on."
-    },
+  "moneyistime": {
+    "name": "Money Is Time",
+    "url": "https://github.com/Noviny/money-is-time",
+    "screenshot": "A screenshot url in an image tag",
+    "what": "This small app allows a person to find out their hourly rate from any kind of salary, and from there discover how many minutes or hours particular purchases take to earn. It can also allow you to stack multiple items to try and discover what you spend the most time earning.",
+    "why": "I made this tiny tool over the Christmas break both to try and help people think about 'value' differently and to practice javascript after a large focus on ruby.",
+    "lesson": "Having a user to test your design, and give you feedback is incredibly important. Thanks to my mum for taking an interest.",
+    "forLater": "This app was designed to be a very small tool and does not need its scope extended. If I wanted to practice my styling, this might be a site worth working on."
+  },
 
-  "socialview" :
-    {
-      "name": "Social View",
-      "url": "https://github.com/Noviny/socialview",
-      "screenshot": "A screenshot url in an image tag",
-      "what": "'Project 2' was a group project, where I teamed up with three of my classmates to create a 'social map'",
-      "why": "Learning to work in a group with other developers is key to success.",
-      "lesson": "Keeping constant track of your intended scope against how much time you have to deliver can greatly relieve pressure.",
-      "forLater": "Optimising how the AJAX requests are made to the two APIs, so the information can be updated live is the next major feature to work on."
-    },
+  "socialview": {
+    "name": "Social View",
+    "url": "https://github.com/Noviny/socialview",
+    "screenshot": "A screenshot url in an image tag",
+    "what": "'Project 2' was a group project, where I teamed up with three of my classmates to create a 'social map'. Using the instagram API and the twitter API, we could display recent photos and tweets on a map around a location, or around a search criteria.",
+    "why": "This project was largely chosen as a way to push a range of development abilities, bringing together a complex project with people working on different parts of the codebase, working with three different APIs, and ",
+    "lesson": "Learning to work in a group with other developers is key to success. It is alright to be frustrated while developing, since code can be very frustrating. The skill is still achieving excellent work through frustration. You will be frustrated either way.",
+    "forLater": "Optimising how the AJAX requests are made to the two APIs, so the information can be updated live is the next major feature to work on."
+  },
 
-  "jsjargon" :
-    {
-      "name": "JS Jargon",
-      "url": "https://github.com/Noviny/socialview",
-      "screenshot": "A screenshot url in an image tag",
-      "why": "There is a lot of jargon used in programming, and we should be looking for ways to make this more accessible.",
-      "lesson": "This was my first time building an app in node, and this greatly enhanced my understanding of it, and gave me a strong base to compare working in node to working on rails.",
-      "forLater": "Integrate the twitter streaming API."
-    }
-}
-
-
-
-// var project = {
-//   "name": "Win Lane, Win Game",
-//   "url": "https://github.com/Noviny/WinLane-WinGame",
-//   "why": "As my first project at General Assembly, we were challenged to digitise a physical game. To challenge myself, I also designed a new game, which was playable with regular playing cards.",
-//   "lesson": "This project taught me a lot on how to structure files within a project to separate concerns, and allow functions to be developed.",
-//   "forLater": "When I go back to this project, I want to add in a multiplayer option, which was beyond the scope of this project."
-// }
+  "jsjargon": {
+    "name": "JS Jargon",
+    "url": "https://github.com/Noviny/socialview",
+    "screenshot": "A screenshot url in an image tag",
+    "why": "There is a lot of jargon used in programming, and we should be looking for ways to make this more accessible.",
+    "lesson": "This was my first time building an app in node, and this greatly enhanced my understanding of it, and gave me a strong base to compare working in node to working on rails.",
+    "forLater": "Integrate the twitter streaming API."
+  }
+};
--- js/routers/AppRouter.js
+++ js/routers/AppRouter.js
@@ -7,24 +7,24 @@ app.AppRouter = Backbone.Router.extend({
     'project/:name': 'viewProject'
   },
 
-  aboutMe: function () {
-    $('#projects-link').removeClass('active')
-    $('#about-me-link').addClass('active')
-    $('#content').html($('#aboutMeTemplate').html())
-    console.log('main page here')
+  aboutMe: function() {
+    $('#projects-link').removeClass('active');
+    $('#about-me-link').addClass('active');
+    $('#content').html($('#aboutMeTemplate').html());
+    console.log('main page here');
   },
 
   // aboutMe: function () {
   //   app.AboutMe.render()
   // }
 
-  projectsIndex: function () {
-    console.log('Projects Index')
-    $('#projects-link').addClass('active')
-    $('#about-me-link').removeClass('active')
-    $('#content').html($('#projects-template').html())
-    app.projectviewmaker = new app.ProjectView()
-    app.projectviewmaker.render()
+  projectsIndex: function() {
+    console.log('Projects Index');
+    $('#projects-link').addClass('active');
+    $('#about-me-link').removeClass('active');
+    $('#content').html($('#projects-template').html());
+    app.projectviewmaker = new app.ProjectView();
+    app.projectviewmaker.render();
   },
 
   // ProjectsIndex: function () {
